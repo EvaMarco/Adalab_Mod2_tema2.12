a5fe3129d8717e5bd0998bd45acd790b8104b236
Migara <evamarcod@gmail.com>
2019-08-05 08:30:45 +0200

    initial commit

---
+++ ej2/js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const body = document.querySelector('.page__main');
+
+fetch('https://api.rand.fun/number/integer?max=100')
+.then(response => response.json())
+.then(data => body.innerHTML = data.result);
